293f7824648c418a38e4e16419f9b54830d4fc81
grace <gracespacedev@gmail.com>
2022-10-17 01:41:24 -0400

    added GameService

---
+++ src/main/java/com/grace/swish/service/GameService.java
@@ -0,0 +1,68 @@
+package com.grace.swish.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.grace.swish.model.Game;
+import com.grace.swish.repository.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	public List<Game> getAllGames() {
+
+		List<Game> games = new ArrayList<Game>();
+		gameRepository.findAll().forEach(games::add);
+		return games;
+
+	}
+	
+	public Optional<Game> getGameById(long gameId) {
+		return gameRepository.findById(gameId);
+		
+	}
+	
+	// not sure why this does not have Optional
+	public Game findGameByTitle(String title) {
+		return gameRepository.findByTitle(title);
+		
+	}
+	
+	public void addGame(Game game) {
+		gameRepository.save(game);
+	}
+	
+	// not sure if i need this method right now
+//	public void updateGame(long gameId, Game game) {
+//		Optional<Game> gameData = gameRepository.findById(gameId);
+//		
+//		if (gameData.isPresent()) {
+//			Game game2 = gameData.get();
+//			game2.setTitle(game.getTitle());
+//			game2.setRating(game.getRating());
+//			game2.setMsrp(game.getMsrp());
+//			game2.setImgUrl(game.getImgUrl());
+//			game2.setGenres(game.getGenres());
+//			game2.setPlatforms(game.getPlatforms());
+//			game2.setFormats(game.getFormats());
+//			game2.setRetailers(game.getRetailers());
+//			gameRepository.save(game2);
+//		}
+//	}
+	
+	public void deleteGame(long gameId) {
+		gameRepository.deleteById(gameId);
+	}
+	
+	public void deleteAllGames() {
+		gameRepository.deleteAll();
+	}
+
+}
