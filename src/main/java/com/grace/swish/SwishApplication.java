32f86bff99bafeeb1479337383bf7ed886e6b846
grace <gracespacedev@gmail.com>
2022-10-18 01:11:47 -0400

    adjusted demo data

--- src/main/java/com/grace/swish/SwishApplication.java
+++ src/main/java/com/grace/swish/SwishApplication.java
@@ -28,38 +28,43 @@ class DemoCommandLineRunner implements CommandLineRunner {
 
 	@Autowired
 	private PlatformRepository platformRepository;
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		Platform platform = new Platform();
-		platform.setPlatformName("PlayStation");
-		
+		Platform platform1 = new Platform();
+		platform1.setPlatformName("Nintendo Switch");
+
 		Platform platform2 = new Platform();
-		platform2.setPlatformName("Xbox");
-		
-		Platform platform3 = new Platform();
-		platform3.setPlatformName("PC");
-		
-		platformRepository.save(platform);
+		platform2.setPlatformName("PC");
+
+		platformRepository.save(platform1);
 		platformRepository.save(platform2);
-		platformRepository.save(platform3);
-		
-		Game game = new Game();
-		game.setTitle("Mario");
-		game.setRating(8);
-		game.setMsrp(59.99);
-		game.setImgUrl("http");
-		game.setPlatforms(Set.of(platform));
-		
-		gameRepository.save(game);
-		
-		
-		
+
+		Game game1 = new Game();
+		game1.setTitle("Mario");
+		game1.setRating(8);
+		game1.setMsrp(59.99);
+		game1.setImgUrl("http");
+		game1.setPlatforms(Set.of(platform1));
+		game1.setDescription(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Vulputate ut pharetra sit amet. Felis eget nunc lobortis mattis aliquam. Rhoncus mattis rhoncus urna neque viverra justo. Ultrices sagittis orci a scelerisque. Libero nunc consequat interdum varius sit amet. Mauris nunc congue nisi vitae suscipit tellus. Consequat id porta nibh venenatis cras sed felis. Enim nulla aliquet porttitor lacus luctus accumsan. Vitae et leo duis ut diam. Turpis in eu mi bibendum neque egestas congue quisque. Quis lectus nulla at volutpat.");
+
+		Game game2 = new Game();
+		game2.setTitle("Wario");
+		game2.setRating(9);
+		game2.setMsrp(59.99);
+		game2.setImgUrl("http");
+		game2.setPlatforms(Set.of(platform1));
+		game2.setDescription(
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl condimentum id venenatis a condimentum vitae sapien pellentesque. Vulputate ut pharetra sit amet. Felis eget nunc lobortis mattis aliquam. Rhoncus mattis rhoncus urna neque viverra justo. Ultrices sagittis orci a scelerisque. Libero nunc consequat interdum varius sit amet. Mauris nunc congue nisi vitae suscipit tellus. Consequat id porta nibh venenatis cras sed felis. Enim nulla aliquet porttitor lacus luctus accumsan. Vitae et leo duis ut diam. Turpis in eu mi bibendum neque egestas congue quisque. Quis lectus nulla at volutpat.");
+
+
+		gameRepository.save(game1);
+		gameRepository.save(game2);
+
 	}
-	
-	
 
 }
